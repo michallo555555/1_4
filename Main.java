38413c3f5853c1f30f7d58446e471bbb9bab9f95
michallo555555 <101186827+michallo555555@users.noreply.github.com>
2022-03-22 12:41:26 +0000

    finish

--- Main.java
+++ Main.java
@@ -1,5 +1,62 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("podaj liczbe: ");
+    System.out.println("1. liczba do potęgi A do B");
+    System.out.println("2. sumy pierwiastków");
+    System.out.println("3. max");
+        System.out.println("4. dzielenie");
+  
+    int liczba = scan.nextInt();
+      System.out.println("podaj liczbe A ");
+      System.out.println("podaj liczbe B ");
+    double A = scan.nextInt();
+    double B = scan.nextInt();
+    switch(liczba) {
+      case 1:
+        double C = A;
+        for (double i=1;i<B;i++) 
+        {
+          C = C*A;
+        }
+        System.out.println(C);
+        break;
+
+      case 2:
+        {
+          double z=Math.sqrt(A);
+          double x=Math.sqrt(B);
+          double c=z+x;
+          System.out.println(c);
+          break;
+        }
+
+      case 3:
+        {
+           System.out.println("wieksza liczba to ");
+          if (A>B)
+          {
+             System.out.println(A);
+            
+            
+          }else
+            System.out.println(B);
+          break;
+            
+        }
+      case 4:
+        {
+             C=A/B;
+         if(B==0){
+          System.out.println("dzielisz przez 0");
+          break;
+        }
+        }
+
+      
+     
+    }
   }
 }
